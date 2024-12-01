23cc8a75410280d5e841b178eb0df459a96fd6a3
bob klaase <bklaase@ethon.nl>
2024-12-01 22:25:00 +0100

    completes day1, moved lib code to use Data.Text

--- lib/AoC/Lib.hs
+++ lib/AoC/Lib.hs
@@ -1,12 +1,14 @@
 module AoC.Lib where
 
 import Data.Time.Clock ( NominalDiffTime, diffUTCTime, getCurrentTime )
-import System.IO ( IOMode(ReadMode), hGetContents, openFile )
 import Text.Printf (printf)
+
+import qualified Data.Text.IO as TI
+import qualified Data.Text as T
 
 data Solution  a b c = Solution
   { day :: Int,
-    parse :: String -> a,
+    parse :: T.Text -> a,
     part1 :: a -> b,
     part2 :: a -> c }
 
@@ -16,12 +18,12 @@ data BenchmarkResult a = BenchmarkResult
 
 solve :: (Show b, Show c) => Solution a b c -> IO ()
 solve solution = do
-  handle <- openFile  (printf "./input/day%d.txt"
-                      (day solution))  ReadMode
-  inputStr <- hGetContents handle
+  inputStr <- TI.readFile  (printf "./input/day%d.txt" (day solution))
   let input = parse solution inputStr
+  
   ans1 <- benchmark $ part1 solution input
   putStrLn $ "Part1: " ++ show ans1
+  
   ans2 <- benchmark $ part2 solution input
   putStrLn $ "Part2: " ++ show ans2
 
--- src/Day1.hs
+++ src/Day1.hs
@@ -1,22 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified AoC.Lib as L
 import qualified Data.Text as T
-import Data.List ( sortOn )
 
-splitWhen :: (a-> Bool) -> [a] -> [[a]]
-splitWhen _ [] = []
-splitWhen p l@(x:xs)
-  | p x = splitWhen p xs
-  | otherwise = group : splitWhen p rest
-      where (group, rest) = break p l
+-- day specific imports
+import Data.List (sort)
 
-parse = splitWhen (== "   ") . lines
+-- parsing input
+parse :: T.Text -> ([Int],[Int])
+parse input =
+  let
+    inputs = map (T.splitOn "   ") $ T.lines input
+    xs = map read ([ T.unpack x | (x:y:_) <- inputs ]) :: [Int]
+    ys = map read ([ T.unpack y | (x:y:_) <- inputs ]) :: [Int]
+  in (xs, ys)
 
-part1 = id
+-- solution part1
+part1 :: ([Int], [Int]) -> Int
+part1 (xs,ys) = sum $ abs <$> zipWith (-) (sort xs) (sort ys)
 
-part2 _ = "0"
+-- solution part2
+part2 :: ([Int], [Int]) -> Int
+part2 (xs,ys) = sum $ map similarityScore xs
+  where
+    similarityScore x = (* x) $ length $ filter (== x) ys 
 
+-- results
 main :: IO ()
 main = do
   let solution = L.Solution 1 parse part1 part2
